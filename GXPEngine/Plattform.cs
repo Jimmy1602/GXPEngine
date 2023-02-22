e2f508835a7917f41b6fc46ac4669a4ad38df5bd
Jimmy1602 <aighter02@gmail.com>
2023-02-22 14:59:15 +0100

    hurensohn

---
+++ GXPEngine/Plattform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GXPEngine;
+using GXPEngine.Core;
+
+public class Plattform : Sprite
+{
+
+    public Plattform(String imageFileName) : base(imageFileName)
+    {
+
+    }
+
+
+}
